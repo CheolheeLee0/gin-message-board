16cd0216c347c93ddbc36800235536c59e790e63
rustin <30518110+Jing-Tian@users.noreply.github.com>
2021-01-29 14:52:39 +0800

    delete 注释

--- main.go
+++ main.go
@@ -1,29 +1,3 @@
-//package main
-//
-//import (
-//	"gin-message-board/controllers"
-//	"gin-message-board/tool"
-//
-//	"github.com/gin-gonic/gin"
-//)
-//
-//func main() {
-//	tool.SqlEngine()
-//
-//	engin := gin.Default()
-//
-//	registRouter(engin)
-//
-//	engin.Run()
-//}
-//
-////调用controller
-//func registRouter(engine *gin.Engine) {
-//	new(controllers.HelloController).Router(engine)
-//	new(controllers.UserController).Router(engine)
-//	new(controllers.MessageController).Router(engine)
-//}
-
 package main
 
 import (
