d0606e4da2f80d8ad440fa8d9fe9fca23c5e501e
rustin <30518110+Jing-Tian@users.noreply.github.com>
2021-01-29 16:47:59 +0800

    controllers 完善

--- controllers/user.go
+++ controllers/user.go
@@ -9,67 +9,67 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// 生成随机的16字符字符串作为会话标记(生产环境不应使用这种方式)
+// GenerateSessionToken 生成随机的16字符字符串作为会话标记(生产环境不应使用这种方式)
 func GenerateSessionToken() string {
-
 	return strconv.FormatInt(rand.Int63(), 16)
 }
 
-// 展示注册页面
+// ShowRegistrationPage 展示注册页面
 func ShowRegistrationPage(c *gin.Context) {
 	render(c, gin.H{"title": "注册"}, "register.html")
 }
 
-// 注册
+// Register 注册页面逻辑
 func Register(c *gin.Context) {
-	// 获取"username"和"password"
+	// 从Post表单获取name="username"和name="password"
 	username := c.PostForm("username")
 	password := c.PostForm("password")
 
-	if err := database.RegisterNewUser(username, password); err == nil {
-		// 如果创建了用户，需要在cookie中设置token，然后登录
-		token := GenerateSessionToken()
-		c.SetCookie("token", token, 3600, "", "", false, true)
-		c.Set("is_logged_in", true)
-
-		render(c, gin.H{"title": "成功注册，登录成功"}, "login-successful.html")
-
-	} else {
+	err := database.RegisterNewUser(username, password)
+	if err != nil {
 		// 如果用户名或密码不合法展示错误再登录界面
 		c.HTML(http.StatusBadRequest, "register.html", gin.H{
 			"ErrorTitle":   "注册失败",
 			"ErrorMessage": err.Error()})
-
 	}
+	token := GenerateSessionToken()
+	// 如果创建了用户，需要在cookie中设置token，然后登录
+	c.SetCookie("token", token, 3600, "", "", false, true)
+	// 设置"is_logged_"=true
+	c.Set("is_logged_in", true)
+
+	render(c, gin.H{"title": "成功注册，登录成功"}, "login-successful.html")
+
 }
 
-// 展示登录界面
+// ShowLoginPage 展示登录界面
 func ShowLoginPage(c *gin.Context) {
 	render(c, gin.H{
 		"title": "登录",
 	}, "login.html")
 }
 
-// 用户登录
+// PerformLogin 执行登录逻辑
 func PerformLogin(c *gin.Context) {
+	// 从Post表单获取name="username"和name="password"
 	username := c.PostForm("username")
 	password := c.PostForm("password")
-
-	if valid, err := database.IsUserValid(username, password); err == nil && valid {
-		token := GenerateSessionToken()
-		c.SetCookie("token", token, 3600, "", "", false, true)
-
-		render(c, gin.H{
-			"title": "成功登录"}, "login-successful.html")
-
-	} else {
+	// 判断是否合法
+	valid, err := database.IsUserValid(username, password)
+	if err != nil || valid {
 		c.HTML(http.StatusBadRequest, "login.html", gin.H{
 			"ErrorTitle":   "登录失败",
 			"ErrorMessage": "Invalid credentials provided"})
 	}
+	// 生成token并设置
+	token := GenerateSessionToken()
+	c.SetCookie("token", token, 3600, "", "", false, true)
+	render(c, gin.H{
+		"title": "成功登录"}, "login-successful.html")
+
 }
 
-// 用户登出
+// Logout 用户登出逻辑
 func Logout(c *gin.Context) {
 	// 设置Cookie token令牌
 	c.SetCookie("token", "", -1, "", "", false, true)
--- database/postgre.go
+++ database/postgre.go
@@ -13,6 +13,7 @@ import (
 
 var db *gorm.DB
 
+// Init 初始化postgresql数据库设置
 func Init() {
 	var dsn = fmt.Sprintf(
 		"host=%s user=%s password=%s dbname=%s port=%d sslmode=disable TimeZone=Asia/Shanghai",
@@ -28,48 +29,63 @@ func Init() {
 		panic(fmt.Errorf("Fatal database error: \n", err))
 	}
 
-	db.AutoMigrate(&models.Message{}, &models.User{})
+	err = db.AutoMigrate(&models.Message{}, &models.User{})
+	if err != nil {
+		panic(err)
+	}
 }
 
+// GetDB 返回数据库指针
 func GetDB() *gorm.DB {
 	return db
 }
 
+// GetAllMessages 从数据库拿取所有留言
 func GetAllMessages() ([]models.Message, error) {
 	var messages []models.Message
 	result := db.Find(&messages)
 	return messages, result.Error
 }
 
+// GetMessageByID 通过ID拿取信息
 func GetMessageByID(id int) (models.Message, error) {
 	var message models.Message
 	result := db.Where("ID = ?", id).First(&message)
 	return message, result.Error
 }
 
+// CreateNewMessage 将新的留言写入数据库
 func CreateNewMessage(title, content string) (models.Message, error) {
 	message := models.Message{Title: title, Content: content}
 	result := db.Create(&message)
 	return message, result.Error
 }
 
+// RegisterNewUser 注册新用户写入数据库
 func RegisterNewUser(username, password string) error {
+	// 判断密码是否为空
 	if strings.TrimSpace(password) == "" {
 		return errors.New("密码不能为空")
-	} else if user_avial, err := IsUsernameAvailable(username); err == nil && !user_avial {
+	}
+	// 判断用户名是否可用
+	userAvailable, err := IsUsernameAvailable(username)
+	if err == nil && !userAvailable {
 		return errors.New("用户名不可用")
 	}
+	// 写入数据库
 	user := models.User{Username: username, Password: password}
 	result := db.Create(&user)
 	return result.Error
 }
 
+// IsUsernameAvailable 判断用户名是否可用
 func IsUsernameAvailable(username string) (bool, error) {
 	var user []models.User
 	result := db.Where("Username = ?", username).Find(&user)
 	return result.RowsAffected == 0, result.Error
 }
 
+// IsUserValid 判断用户是否存在,合法
 func IsUserValid(username, password string) (bool, error) {
 	var user []models.User
 	result := db.Where("Username = ? AND Password = ?", username, password).Find(&user)
